a5295af9dd4a142b2d728151e5950c92a2f88e90
Natalitta <nataliialebedieva@gmail.com>
2023-06-23 20:35:23 +0000

    Add complex reduce method examples.

--- reduce.js
+++ reduce.js
@@ -8,7 +8,7 @@
 // Summing an array of numbers:
 let nums = [1, 2, 3, 4, 5];
 let sum = nums.reduce((acc, curr) => acc + curr, 0);
-console.log(sum);
+//console.log(sum);
 
 const teamMembers = [
   {
@@ -35,5 +35,66 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExp = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExp);
 
 // Grouping by a property, and totaling it too
+
+//Developer: 12, Designer: 4 - this is the result we expect
+let expByProf = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(expByProf);
+
+//Challenge
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = students.reduce((acc, cur) => {
+  acc = acc.max > cur.results.english ? acc: {name:cur.name, max:cur.results.english};
+  return acc;
+  }, {name: '', max: 0});
+console.log(biggest);
+
+/* Using destructuring 
+const biggest = students.reduce(({max, name}, {name:n, results:{english}}) => {
+    if(max < english) {
+        acc = {name:n, max: english};
+    }
+    return acc;
+    }, {name: '', max: 0});
+    
+console.log(biggest);
+*/
